bbf9e7805daa8b99f5186d1548918e487196c9fd
Skylinker <skylinkertu33@gmail.com>
2021-02-22 14:35:02 +0700

    Added (NPM-package): Install npm-packages to "SVG-icons":
    - Added setting of work of packages for svg files
    - "svgo": "^1.1.0",
    - "svgo-loader": "^2.1.0",
    - "vue-cli-plugin-svg-sprite": "~1.0.0"

--- client/vue.config.js
+++ client/vue.config.js
@@ -25,8 +25,37 @@ module.exports = {
     }
   },
   chainWebpack: config => {
-    config.resolve.alias.set('@', path.resolve('src')) 
+    config.resolve.alias.set('@', path.resolve('src'))
+		config.module
+            .rule('svg-sprite')
+            .use('svgo-loader')
+            .loader('svgo-loader');
   },
+	pluginOptions: {
+		svgSprite: {
+			/*
+			 * The directory containing your SVG files.
+			 */
+			dir: 'src/assets/svg-icons',
+			/*
+			 * The reqex that will be used for the Webpack rule.
+			 */
+			test: /\.(svg)(\?.*)?$/,
+			/*
+			 * @see https://github.com/kisenka/svg-sprite-loader#configuration
+			 */
+			loaderOptions: {
+					extract: true,
+					spriteFilename: 'img/icons.[hash:8].svg' // or 'img/icons.svg' if filenameHashing == false
+			},
+			/*
+			 * @see https://github.com/kisenka/svg-sprite-loader#configuration
+			 */
+			pluginOptions: {
+					plainSprite: true
+			}
+		}
+	},
   devServer: {
     historyApiFallback: true,
     proxy: 'http://localhost:8081'
