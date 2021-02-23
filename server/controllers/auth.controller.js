ab89d609f141a6dd5b6ff2eb985c2724fc74c0cb
Skylinker <skylinkertu33@gmail.com>
2021-02-23 18:00:02 +0700

    Added (server):
    - Installed a package for encrypting passwords
    - Written encrypted password authorization
    - Added/change various messages about errors of authorization

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 const { sequelize, user, task } = require('../db/models/');
 const jsonwebtoken = require('jsonwebtoken');
+const bcrypt = require('bcrypt')
 
 function genJWTToken({ id, login }) {
 	return jsonwebtoken.sign({
@@ -12,12 +13,21 @@ function genJWTToken({ id, login }) {
 module.exports.login = (req, res) => {
 	user.findOne({
 		where: {
-			login: req.body.login,
-			password: req.body.password
+			login: req.body.login
 		},
-		attributes: ['id', 'name', 'login', 'avatar', 'admin']
+		attributes: ['id', 'name', 'login', 'avatar', 'admin', 'password']
 	}).then(user => {
 		if (user) {
+			const passIsValid = bcrypt.compareSync(req.body.password, user.password)
+
+			if (!passIsValid)
+				return res.status(401).send({
+					error: 'Неверный логин или пароль',
+					token: null
+				});
+
+			delete user.password;
+
 			const token = genJWTToken({ id: user.id, login: user.login });
 
 			if (token)
@@ -27,10 +37,9 @@ module.exports.login = (req, res) => {
 					timestamp: new Date(),
 					token: `Bearer ${token}`
 				});
-		}
-		else
+		} else
 			res.status(404).send({
-				warning: 'Not found!',
+				error: 'Данный пользователь не найден!'
 			});
 	}).catch(err => res.status(500).send({
 		msg: 'Error!',
@@ -39,14 +48,18 @@ module.exports.login = (req, res) => {
 };
 
 module.exports.register = (req, res) => {
-	if (req.body.password !== req.body.passwordConfirm) return res.status(400);
+	if (req.body.password !== req.body.passwordConfirm)
+		return res.status(401).send({ error: 'Неверная пара: "пароль, повторите пароль"', token: null });
 
 	user.create({
 		login: req.body.login,
-		password: req.body.password,
+		password: bcrypt.hashSync(req.body.password, 8),
+		admin: 0,
 		name: ''
 	}).then(user => {
 		const token = genJWTToken({ id: user.id, login: user.login });
+
+		delete user.password;
 
 		if (token)
 			res.status(200).send({
